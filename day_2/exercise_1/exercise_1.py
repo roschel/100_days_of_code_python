728d351443e174da135616c4dfec1306fbae2e09
roschel <joaomroschel@gmail.com>
2022-06-07 17:45:49 -0300

    day_2 - exercise 1 and 2

---
+++ day_2/exercise_1/exercise_1.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+num_1 = int(two_digit_number[0])
+num_2 = int(two_digit_number[1])
+sum_numbers = num_1 + num_2
+
+print(sum_numbers)
